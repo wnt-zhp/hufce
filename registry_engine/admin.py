d1093b790a0ccc16022d9d3fe8feaba57af100be
Jacek Bzdak <jbzdak@gmail.com>
2012-02-11 00:55:06 +0100

    Delete old history engine

--- registry_engine/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-
-__author__ = 'jb'
-
-class HistoryAdminInline(admin.TabularInline):
-    extra = 0
-    can_delete = False
-    max_num = 0
